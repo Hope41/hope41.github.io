0834e4c9fc7faf9a8a82e8f206430b7c065c68b2
Hope41 <87899147+Hope41@users.noreply.github.com>
2021-07-25 16:47:30 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,9 @@
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'G-F1R0H0BSJE');
-
 oncontextmenu = e => e.preventDefault()
 
 onload = () => {
 	let code = document.getElementsByTagName('pre')
 
-	for (item of code) {
+	for (let item of code) {
 		item.innerHTML = item.innerHTML
 		.replace(/\/\/.+\n/g, a => '<span class=note>' + a + '</span>')
 		.replace(/("|'|`).+("|'|`)/g, a => '<span class=string>' + a + '</span>')
