12c81e260500ba3ee5aa28c8e26bd87e1f522c31
Hope41 <joachimford@hotmail.com>
2023-08-27 20:26:26 +0100

    The Sixth Great Update (or was it the fifth?)

--- js/main.js
+++ js/main.js
@@ -7,67 +7,19 @@ function bar() {
 }
 
 onload = () => {
-    const image = document.querySelectorAll('.image')
     const iframe = document.querySelectorAll('.iframe')
     const code = document.querySelectorAll('.code')
     const year = document.querySelector('.year')
+
     const load = async text => await (await fetch(text)).text()
     if (year) year.textContent = new Date().getFullYear()
 
     iframe.forEach(item => {
         const source = item.getAttribute('source')
-        const link = item.getAttribute('link')
-
         const iframe = document.createElement('iframe')
         iframe.src = source
 
-        if (link) {
-            iframe.className = 'play'
-
-            const text = document.createElement('div')
-            const play = document.createElement('i')
-            const a = document.createElement('a')
-
-            a.href = link
-            a.target = '_blank'
-        
-            play.className = 'fa-solid fa-play'
-            text.innerHTML = '<span class = italic>Click on the box to go fullscreen</span>'
-        
-            item.appendChild(text)
-            item.appendChild(iframe)
-            item.appendChild(play)
-            item.appendChild(a)
-        }
-        else item.appendChild(iframe)
-    })
-
-    image.forEach(item => {
-        const source = item.getAttribute('source')
-        const link = item.getAttribute('link')
-
-        const img = document.createElement('img')
-        img.src = source
-
-        if (link) {
-            img.className = 'play'
-
-            const text = document.createElement('div')
-            const play = document.createElement('i')
-            const a = document.createElement('a')
-
-            a.href = link
-            a.target = '_blank'
-
-            play.className = 'fa-solid fa-play'
-            text.innerHTML = '<span class = italic>Click on the box to go fullscreen</span>'
-        
-            item.appendChild(text)
-            item.appendChild(img)
-            item.appendChild(play)
-            item.appendChild(a)
-        }
-        else item.appendChild(img)
+        item.appendChild(iframe)
     })
 
     code.forEach(async item => {
